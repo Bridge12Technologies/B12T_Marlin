79aa6ad26e500d42dc56918d96933aa8521fdfe1
Yen-Chun Huang <yhuang@bridge12.com>
2024-01-11 15:01:05 -0500

    Change the axis steps per mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1234,7 +1234,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 5468, 5468, 5468, 5468 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 5249.3438, 5249.3438, 5249.3438, 5249.3438 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
