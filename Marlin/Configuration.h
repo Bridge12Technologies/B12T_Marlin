81337caca17d3fdb08fbf6b1f21e234a673962c1
Yen-Chun Huang <yhuang@bridge12.com>
2024-01-16 10:39:58 -0500

    Change the feedrate for homing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2263,7 +2263,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (2*60), (2*60), (2*60) }
+#define HOMING_FEEDRATE_MM_M { (0.5*60), (0.5*60), (0.5*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
